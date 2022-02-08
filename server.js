c878a1ddb1be52f7cc7c4ac6e49e3ce220caecb2
Your Name <you@example.com>
2022-02-08 17:23:49 +0400

    message

--- server.js
+++ server.js
@@ -10,6 +10,10 @@ app.use(express.static("."));
 app.get('/', function (req, res) {
     res.redirect('index.html');
 });
+
+app.get('/start', function (req, res) {
+    res.redirect('start.html');
+});
 server.listen(3000, function () {
     console.log("runed on port:3000");
 });
